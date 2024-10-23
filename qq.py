64273111311d80bab49605a1f3bb9cfa335160c3
Podal18 <118309400+Podal18@users.noreply.github.com>
2024-10-23 10:51:53 +0300

    Update qq.py

--- qq.py
+++ qq.py
@@ -1,45 +1,67 @@
+
+
+
 class Man:
     def __init__(self, name, age):
         self.__name = name
         self.__age = age
 
-    def set_age(self, age):
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
         if age > 0 and age < 110:
             self.__age = age
         else:
             print("НЕдопустимое значение")
 
-    def get_age(self):
-        return self.__age
-
-    def get_name(self):
+    @property
+    def name(self):
         return self.__name
 
-    def prnt_info(self):
-        print(f"Имя - {self.__name}\nВозраст - {self.__age}")
+    # def __repr__(self): для красивоо выводав консоль
+    def __str__(self):
+        return f"Имя - {self.__name}\nВозраст - {self.__age}"
 
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __add__(self, other):
+        return [self, other]
 
 class Worker(Man):
 
-    def __init__(self, name):
-        self.name = name
+    # def __init__(self, name):
+    #     self.name = name
 
     def __del__(self):
+        # print(self.__dict__)
+        # print(f"Пропал человек с именем {self._Man__name}")
         print(f"Пропал человек с именем {self.name}")
 
-
 class coaches(Man):
-    pass
+
+    def __init__(self, qualification):
+        self.qualiffication = qualification
 
 
-# pt1 = Man("Александра", 20)
-# pt1.prnt_info()
-# pt1.set_age(888)
-# pt1.set_age(19)
-# pt1.prnt_info()
 
-pt2 = Worker("Семен")
-pt2.prnt_info()
 
-# pt3 = coaches("Андрей", 19)
-# pt3.prnt_info()
+
+pt1 = Man("Семен", 20)
+print(pt1)
+pt1.age = 999
+pt1.age = 21
+print(pt1)
+
+pt2 = Worker("Семен", 20)
+print(pt2)
+# print(pt2._Man__name)
+print(pt2.name)
+
+pt3 = coaches("Андрей", 19)
+print(pt3)
+
+print(*(pt1 + pt2))
